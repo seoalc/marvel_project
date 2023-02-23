061f01395076954493cdf386af371728405c234c
unknown <rrryyy07@gmail.com>
2023-02-23 16:15:28 +0600

    offset to 210

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -11,7 +11,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1562,
+        offset: 210,
         charEnded: false
     }
     
